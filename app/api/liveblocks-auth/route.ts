03dd8df0f1491b226719673b9a25c8186f1fb579
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2024-02-17 14:43:59 -0500

    Started Adding Liveblocks

---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,26 @@
+import { Liveblocks } from '@liveblocks/node';
+import { useUser, auth } from '@clerk/nextjs';
+
+const liveblocks = new Liveblocks({
+  secret: process.env.LIVEBLOCKS_SECRET_DEV_API_KEY!,
+});
+
+export async function POST(req: Request) {
+  const { user } = useUser();
+  const { orgId } = auth();
+
+  if (!orgId || !user) return new Response('Unauthorized', { status: 401 });
+
+  // Start an auth session inside your endpoint
+  const session = liveblocks.prepareSession(user.id);
+
+  // Implement your own security, and give the user access to the room
+  const { room } = await req.json();
+  if (room) {
+    session.allow(room, session.FULL_ACCESS);
+  }
+
+  // Authorize the user and return the result
+  const { status, body } = await session.authorize();
+  return new Response(body, { status });
+}
