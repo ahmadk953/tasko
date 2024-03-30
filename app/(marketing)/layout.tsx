81603a9e2be467b7f8bc45121db5a4979b6142bf
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2024-03-29 23:33:28 -0400

    Google Verification Stuff

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -4,6 +4,10 @@ import { Navbar } from './_components/navbar';
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='h-full bg-slate-100'>
+      <meta
+        name='google-site-verification'
+        content='PRxg9VJRF6bNC8Gn2foGdrSvXjqihsgL4w9HPTt5nVk'
+      />
       <Navbar />
       <main className='bg-slate-100 pb-20 pt-40'>{children}</main>
       <Footer />
