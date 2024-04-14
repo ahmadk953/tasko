7127644c9ee8cc5305520195492156a98bfe3d62
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2024-04-14 17:17:01 -0400

    Removed Old Middleware

--- middleware.ts
+++ middleware.ts
@@ -8,28 +8,3 @@ export default clerkMiddleware();
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
-
-/**
- * Old Middleware
- */
-
-/* import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
-import { NextResponse } from 'next/server';
-
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhook', '/privacy-policy', '/terms-of-service'],
-  afterAuth(auth, req) {
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-
-    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== '/select-org') {
-      const orgSelection = new URL('/select-org', req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-  },
-});
-
-export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}; */
