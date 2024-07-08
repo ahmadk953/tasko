c11f20c62712d7e9dc4feec29d626f6f98a58654
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2024-07-07 20:04:37 -0400

    Updated Liveblocks Config

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -4,8 +4,7 @@ declare global {
   interface Liveblocks {
     // Each user's Presence, for useMyPresence, useOthers, etc.
     Presence: {
-      // Example, real-time cursor coordinates
-      // cursor: { x: number; y: number };
+      cursor: { x: number; y: number } | null;
     };
 
     // The Storage tree for the room, for useMutation, useStorage, etc.
