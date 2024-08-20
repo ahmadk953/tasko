72908920762952f41739e9f8408c3b26fea3aca7
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2024-08-20 14:32:15 -0400

    Removed Prisma Opimize and Updated package.json

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,8 @@
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
-import { withOptimize } from '@prisma/extension-optimize';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = new PrismaClient()
-  .$extends(withAccelerate())
-  .$extends(withOptimize());
+export const db = new PrismaClient().$extends(withAccelerate());
