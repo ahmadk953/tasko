a967d989c308a3978ad8ad2e0798272adda151d9
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2024-08-20 14:24:59 -0400

    Added Prisma Optimize

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,11 @@
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { withOptimize } from '@prisma/extension-optimize';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = new PrismaClient().$extends(withAccelerate());
+export const db = new PrismaClient()
+  .$extends(withAccelerate())
+  .$extends(withOptimize());
