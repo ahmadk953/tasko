a9bd470c68e98132a310cbe9d2a833dc4a3a11f1
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2024-12-23 14:14:15 -0500

    Fixed Next.js Middleware, Added API Route Caching, and Removed Unused Packages

--- app/api/cards/[cardId]/logs/route.ts
+++ app/api/cards/[cardId]/logs/route.ts
@@ -29,7 +29,14 @@ export async function GET(
       take: 3,
     });
 
-    return NextResponse.json(auditLogs);
+    return new NextResponse(JSON.stringify(auditLogs), {
+      status: 200,
+      headers: {
+        'Cache-Control': 'public, s-maxage=5',
+        'CDN-Cache-Control': 'public, s-maxage=60',
+        'Vercel-CDN-Cache-Control': 'public, s-maxage=3600',
+      },
+    });
   } catch (error) {
     return new NextResponse(JSON.stringify(error), { status: 500 });
   }
--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -34,7 +34,14 @@ export async function GET(
       },
     });
 
-    return NextResponse.json(card);
+    return new NextResponse(JSON.stringify(card), {
+      status: 200,
+      headers: {
+        'Cache-Control': 'public, s-maxage=1',
+        'CDN-Cache-Control': 'public, s-maxage=60',
+        'Vercel-CDN-Cache-Control': 'public, s-maxage=3600',
+      },
+    });
   } catch (error) {
     return new NextResponse(JSON.stringify(error), { status: 500 });
   }
