a35fdff29fbd99b11cd7ae935bb16c9e3744e7c5
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2024-12-27 18:24:19 -0500

    Add Bot Protection Using Arcjet

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,19 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
-import arcjet, { shield } from './lib/arcjet';
+import arcjet, { shield, detectBot } from './lib/arcjet';
 import { createMiddleware } from '@arcjet/next';
 
-const aj = arcjet.withRule(
-  shield({
-    mode: 'LIVE',
-  })
-);
+const aj = arcjet
+  .withRule(
+    shield({
+      mode: 'LIVE',
+    })
+  )
+  .withRule(
+    detectBot({
+      mode: 'LIVE',
+      allow: ['CATEGORY:SEARCH_ENGINE'],
+    })
+  );
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
