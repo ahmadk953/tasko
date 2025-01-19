4caba6b18692925f809eb32fb1af3e3ddc05c460
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2025-01-19 16:49:02 -0500

    Added Sentry Redis Integration

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -24,7 +24,7 @@ Sentry.init({
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for tracing.
   // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.5,
   // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
   tracePropagationTargets: ['localhost', /^https:\/\/tasko\.ahmadk953\.org/],
 
@@ -33,7 +33,7 @@ Sentry.init({
   // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
   // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
   // result in 25% of transactions being profiled (0.5*0.5=0.25)
-  profilesSampleRate: 1.0,
+  profilesSampleRate: 0.5,
 
   // Define how likely Replay events are sampled.
   replaysSessionSampleRate: 0.25,
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -11,7 +11,7 @@ Sentry.init({
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for tracing.
   // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.5,
   // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
   tracePropagationTargets: ['localhost', /^https:\/\/tasko\.ahmadk953\.org/],
 
@@ -20,7 +20,7 @@ Sentry.init({
   // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
   // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
   // result in 25% of transactions being profiled (0.5*0.5=0.25)
-  profilesSampleRate: 1.0,
+  profilesSampleRate: 0.5,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
