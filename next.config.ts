be70b889255bd5b0ace830585c733feca909df93
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2025-01-19 01:14:55 -0500

    Fix Build Issues

--- next.config.ts
+++ next.config.ts
@@ -53,7 +53,7 @@ const nextConfig: NextConfig = {
   },
   cacheHandler:
     process.env.NODE_ENV === 'production'
-      ? require.resolve('./cache-handler.mts')
+      ? require.resolve('./cache-handler.mjs')
       : undefined,
 };
 
