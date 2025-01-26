5b578ff9b5f4585daf1df9936252f8609daa61a3
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2025-01-26 16:13:41 -0500

    Fix Compile Issues (once and for all)

--- cache-handler.mjs
+++ cache-handler.mjs
@@ -1,4 +1,14 @@
-import redis from './lib/redis.js';
+import Redis from 'ioredis';
+
+let redis;
+
+if (!redis) {
+  redis = new Redis(process.env.REDIS_URL);
+
+  redis.on('error', (err) => {
+    console.error('Redis Client Error:', err);
+  });
+}
 
 export default class CacheHandler {
   constructor(options) {
