bdc28f40c85e85799b792d691b26a5fda55ed139
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2025-02-11 21:21:20 -0500

    Upgrade to Sentry v9

--- next.config.ts
+++ next.config.ts
@@ -88,9 +88,6 @@ export default withContentCollections(
     // side errors will fail.
     tunnelRoute: '/monitoring',
 
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
 
