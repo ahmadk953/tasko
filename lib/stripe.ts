abf88e64588a0e890e4f51645e1004ec078c516e
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2025-02-25 20:34:40 -0500

    Updated Stripe API Version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
-  apiVersion: '2025-01-27.acacia',
+  apiVersion: '2025-02-24.acacia',
   typescript: true,
 });
