375f55140447f5896e66d16d181611f71f8b8b28
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2025-11-06 23:19:45 +0000

    Sentry logging config

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: 'https://bb697105eaabbc6f70af12e84e936ded@o4508368569368576.ingest.us.sentry.io/4508368582017024',
 
-  integrations: [Sentry.prismaIntegration()],
+  integrations: [Sentry.prismaIntegration(), Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 0.25,
